a7c206987462328e571276ed4847400aed2f9c44
Shingokun <hoanglong0338@gmail.com>
2022-08-07 18:37:46 +0700

    add  TestToFinishc#/

---
+++ TestToFinishc#/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace TestToFinishC
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+         Loop();
+         Console.ReadKey();
+    }
+    static void Loop()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        
+        int j=0;
+
+        for ( int i =0; i <50 ; i ++,j++)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("đm tin =))");
+            Console.WriteLine("IQ của tin sau mỗi một năm:");
+            Console.WriteLine("IQ năm {0} :  {1} ",i,50-j);
+
+           
+            
+
+
+        }
+        
+
+    }
+  }
+
+}
